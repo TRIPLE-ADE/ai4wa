12b67deb82a31d2b72bb1746010d75d01b6d5d4e
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-11-29 13:42:31 +0100

    Initial commit: Implement landing page

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta description="AI" content="AI">
+    <meta description="ai4fs.com.ng" content="AI4FS">
+    <meta description="ai4fs" content="ai4fs">
+    <meta description="artificial Intelligence" content="artificial Intelligence for female in stem">
+    <meta description="female in STEM" content="female in stem">
+    <title>AI4WA | Home</title>
+    <!-- Fav Icon -->
+    <link rel="icon" href="assets/images/AI4FS-icon.png" sizes="37x37" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <!-- Stylesheets -->
+    <link href="assets/css/font-awesome-all.css" rel="stylesheet">
+    <link href="assets/css/flaticon.css" rel="stylesheet">
+    <link href="assets/css/owl.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/color.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap"
+        rel="stylesheet">
+</head>
+
+<!-- page wrapper -->
+
+<body>
+    <div class="boxed_wrapper">
+        <!-- main header -->
+        <?php @include './layout/header.php' ?>
+        <!-- main-header end -->
+
+        <!-- Mobile Menu  -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <div class="close-btn"><i class="fas fa-times"></i></div>
+            <nav class="menu-box" style="background:#2a9735;">
+                <div class="nav-logo">
+                    <a href="index.php">
+                        <img src="assets/images/logo.jpeg" alt="" title="">
+                    </a>
+                </div>
+                <div class="menu-outer">
+                </div>
+                <div class="contact-info">
+                    <h4>Contact Info</h4>
+                    <ul>
+                        <li><a href="tel:+234"></a></li>
+                        <li><a href="mailto:info@example.com"></a></li>
+                    </ul>
+                </div>
+                <div class="social-links">
+                    <ul class="clearfix">
+                        <li><a href="index.php"><span class="fab fa-twitter"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-facebook-square"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-instagram"></span></a></li>
+                        <li><a href="index.php"><span class="fab fa-youtube"></span></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!-- End Mobile Menu -->
+
+        <!-- banner-section -->
+        <section class="banner-style-two" id="home">
+            <div class="auto-container">
+                <div class="clearfix row">
+                    <div class="col-lg-6 col-md-12 col-sm-12 content-column d-flex flex-column justify-content-center align-items-center">
+                        <div class="content-box">
+                            <h2>AI for Women in Aquaculture</h2>
+                            <p>Artificial Intelligence for Women in Aquaculture (AI4WA) seeks to create a focal point for women in the field of Artificial Intelligence (AI) within the context of aquaculture, specifically in Nigeria, aligning with Sustainable Development Goals (SDGs).</p>
+                            <!-- <div class="btn-box">
+                                <a href="about.php" class="theme-btn btn-five">Learn More</a>
+                            </div> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 image-column">
+                        <div class="pt-5 pb-5">
+                            <figure class="image" style="display:flex; justify-content:center;">
+                                <img src="./assets/images/hero.jpeg" alt="" width="90%" class="rounded-lg"/>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- banner-section end -->
+
+        <!-- Login Section -->
+        <section id="about">
+            <div class="login-section">
+                <h2>Already have an account?</h2>
+                <p>Log in to your Agricultural Management System to access powerful tools.</p>
+                <a href="your-login-page.html" class="login-btn">Login Now</a>
+            </div>
+        </section>
+
+        <section class="faq" id="faq">
+            <h2>Frequently asked Questions</h2>
+            <p>Got questions? We have answers</p>
+            <div class="faq-container">
+                <div class="faq-item">
+                    <div class="faq-question">What is the Agricultural Management System (AMS) about?</div>
+                    <div class="faq-answer">AMS is a comprehensive platform designed to streamline agricultural
+                        management tasks, including crop monitoring, resource allocation, and data analysis.</div>
+                </div>
+                <div class="faq-item">
+                    <div class="faq-question">How can AMS benefit my farm or agricultural business?</div>
+                    <div class="faq-answer">AMS can enhance efficiency, increase productivity, and provide valuable
+                        insights to optimize your agricultural operations. It offers tools for better decision-making
+                        and resource management.</div>
+                </div>
+                <div class="faq-item">
+                    <div class="faq-question">Is my data secure with AMS?</div>
+                    <div class="faq-answer">Yes, we take data security seriously. AMS employs state-of-the-art
+                        encryption and security measures to protect your information.</div>
+                </div>
+            </div>
+        </section>
+        <!-- main-footer -->
+        <?php @include "./layout/footer.php" ?>
+        <!-- main-footer end -->
+    </div>
+    <!-- jequery plugins -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/validation.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/scrollbar.js"></script>
+    <script src="assets/js/tilt.jquery.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/bxslider.js"></script>
+    <!-- main-js -->
+    <script src="assets/js/script.js"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const faqQuestions = document.querySelectorAll('.faq-question');
+
+            faqQuestions.forEach(function (question) {
+                question.addEventListener('click', function () {
+                    const answer = this.nextElementSibling;
+
+                    if (answer.style.display === 'block') {
+                        answer.style.display = 'none';
+                        this.classList.remove('open');
+                    } else {
+                        answer.style.display = 'block';
+                        this.classList.add('open');
+                    }
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>
